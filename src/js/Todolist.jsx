e353b67823e3a140037cfa6f83aae82ed37c9685
Michelle <mw.michiko.85@gmail.com>
2018-03-07 23:56:40 +0000

    first push to my own repo

--- src/js/Todolist.jsx
+++ src/js/Todolist.jsx
@@ -6,46 +6,42 @@ export class Todolist extends React.Component {
         //everything goes inside a big div
         return (
             <div className="container">
-              <div className="row">
-                <div className= "col-sm-12 col-md-6">
-                  <div className="card">
-                    <div className="card-header text-center listTitle">Sunday Shopping List</div>
-                        <div className="card-block">
-                            <form>
-                              <div className="input-group">
-                                <input type="text" className="form-control" placeholder="add tasks" aria-label="Recipient's username" aria-describedby="basic-addon2"/>
-                                    <div className="input-group-append">
-                                        <button className="btn btn-outline-secondary" type="button">ADD</button>
+                <div className="row">
+                    <div className= "col-sm-12 col-md-6">
+                        <div className="card">
+                            <div className="card-header text-center listTitle">Sunday Shopping List</div>
+                                <div className="card-block">
+                                    <form>
+                                        <div className="input-group">
+                                            <input type="text" className="form-control" placeholder="add tasks" aria-label="Recipient's username" aria-describedby="basic-addon2"/>
+                                                <div className="input-group-append">
+                                                    <button className="btn btn-outline-secondary" type="button">ADD</button>
+                                                </div>
+                                         </div>
+                                    </form>
+                                    <div className="row">
+                                        <div className="col-md-12">
+                                           <ul className="list-group">
+                                                <li className="list-group-item d-flex justify-content-between align-items-center">
+                                                Onions
+                                                    <label className="btn btn-outline-secondary"><i className="fas fa-trash-alt"></i></label>
+                                                </li>
+                                                <li className="list-group-item d-flex justify-content-between align-items-center">
+                                               mushrooms
+                                                     <label className="btn btn-outline-secondary"><i className="fas fa-trash-alt"></i></label>
+                                                 </li>
+                                                 <li className="list-group-item d-flex justify-content-between align-items-center">
+                                                almond milk
+                                                    <label className="btn btn-outline-secondary"><i className="fas fa-trash-alt"></i></label>
+                                                 </li>
+                                            </ul>
+                                        </div>
                                     </div>
-                              </div>
-                            </form>
-                                <div className="row">
-                                    <div className="col-md-12">
-                                       <ul className="list-group">
-                                          <li className="list-group-item d-flex justify-content-between align-items-center">
-                                            Onions
-                                           <button className="btn btn-outline-secondary" type="button">X</button>
-                                          </li>
-                                          <li className="list-group-item d-flex justify-content-between align-items-center">
-                                           mushrooms
-                                            <button className="btn btn-outline-secondary" type="button">X</button>
-                                          </li>
-                                          <li className="list-group-item d-flex justify-content-between align-items-center">
-                                            almond milk
-                                            <button className="btn btn-outline-secondary" type="button">X</button>
-                                          </li>
-                                        </ul>
-                                        
-                                          
-                                    
-                                     
-                                    </div>
-                                </div>
-                        </div>        
-                    </div> 
+                                </div>        
+                            </div> 
+                        </div>
+                    </div>
                 </div>
-            </div>
-         </div>
             
         );
     }
