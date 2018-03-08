5e1c6238c31422f5b7657c4c826a66d646222eb5
Michelle <mw.michiko.85@gmail.com>
2018-03-08 00:35:35 +0000

    next change made now it shows  a different list but closer to the goal

--- src/js/Todolist.jsx
+++ src/js/Todolist.jsx
@@ -2,7 +2,28 @@ import React from 'react';
 
 export class Todolist extends React.Component {
     
+    constructor(){
+        super();
+      //object this.state then element is todos followed by an array of objects (title) there are two states todos and taskinput  
+        this.state = { 
+          todos: [
+            { title: 'Make the bed', id: (Math.random()*10)},
+            { title: 'Wash my hands', id: (Math.random()*10)},
+            { title: 'Eat', id: (Math.random()*10)},
+            { title: 'Walk the dog', id: (Math.random()*10)}
+          ],
+          taskInput: ''
+        };
+    }
     render() {
+        let theList = this.state.todos.map((currentElement, index) =>{
+            return(
+         <li className="list-group-item d-flex justify-content-between align-items-center">
+            XXXXX
+                <label className="btn btn-outline-secondary"><i className="fas fa-trash-alt"></i></label>
+            </li>
+        )
+       });
         //everything goes inside a big div
         return (
             <div className="container">
@@ -19,21 +40,11 @@ export class Todolist extends React.Component {
                                                 </div>
                                          </div>
                                     </form>
-                                    <div className="row">
+                                    < div className="row">
                                         <div className="col-md-12">
                                            <ul className="list-group">
-                                                <li className="list-group-item d-flex justify-content-between align-items-center">
-                                                Onions
-                                                    <label className="btn btn-outline-secondary"><i className="fas fa-trash-alt"></i></label>
-                                                </li>
-                                                <li className="list-group-item d-flex justify-content-between align-items-center">
-                                               mushrooms
-                                                     <label className="btn btn-outline-secondary"><i className="fas fa-trash-alt"></i></label>
-                                                 </li>
-                                                 <li className="list-group-item d-flex justify-content-between align-items-center">
-                                                almond milk
-                                                    <label className="btn btn-outline-secondary"><i className="fas fa-trash-alt"></i></label>
-                                                 </li>
+                                           {theList}
+                                              
                                             </ul>
                                         </div>
                                     </div>
